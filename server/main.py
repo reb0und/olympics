c61fb068b1a3d7df64b54c922311dc3896e7dc4c
# <dmagats123@gmail.com>
2023-06-28 13:52:59 -0700

    feat: add room routes, etc

--- server/main.py
+++ server/main.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, render_template, redirect, request, Response
+from db import RoomClient
+from session import SessionClient
+from uuid import uuid4
 
 app = Flask(__name__, static_folder='../web/static', template_folder='../web/static/')
+room_client = RoomClient(0)
+session_client = SessionClient(1)
 
-@app.route('/')
+def apply_headers(resp: Response) -> Response:
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET'
+    resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    resp.headers['Access-Control-Max-Age'] = '3600'
+    resp.headers['Content-Type'] = 'text/html'
+    return resp
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    resp = make_response(render_template('index.html'))
 
-if __name__ == "__main__":
+    apply_headers(resp)
+    
+    resp.headers['Session-id'] = session_client.create_session(uuid4, request.remote_addr)
+
+    return resp
+
+@app.route('/room/', methods=['GET'])
+def room():
+    return redirect('/room/' + room_client.create_room(), 301)
+
+# @app.route('/room/<room_id>', methods=['GET'])
+# def room():
+#     return render_template('room.html')
+
+if __name__ == '__main__':
     app.run(debug=True, port=3000)
