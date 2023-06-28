b359370180705b72d66642f6304f9c60deb2bd94
skparab1 <skparab1@gmail.com>
2023-06-28 14:02:36 -0700

    feat: badminton and soccer games
    Co-authored-by: veerals <veerals@users.noreply.github.com>
    Co-authored-by: Praval Chaudhary <Chaudhary-Prval888@users.noreply.github.com>

--- game/soccer/src/soccer.js
+++ game/soccer/src/soccer.js
@@ -3,9 +3,21 @@
 let actkey = event.code.replace('Key','')
 let player1x = 100;
 let player1y = 50;
-
 let player2x = 0;
 let player2y = 75;
+let soccerBallx = 10;
+let soccerBally = 20;
+let ballz = 50;
 
+function keyPressed(){
+    if (actkey == '1'){
+        player1y -= 3;
+        player.style.transform = "rotate(-50deg)";
+        p1angle = -50;
+    }
+    else if ( actkey == '2'){
+        
+    }
+}
 
     
