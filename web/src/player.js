ec92684afe58a5874c1027a602dbc2cdb2aca12a
# <dmagats123@gmail.com>
2023-06-28 20:19:35 -0700

    refactor: move game files to web

---
+++ web/src/player.js
@@ -0,0 +1,21 @@
+
+// let obj = new PLayers();
+
+// function Players() {
+//     let speed = 0;
+//     let strength = 0;
+//     let energy = 0;
+//     let name = "";
+//   }
+  
+
+class Player {
+  constructor(name, speed, strength, energy) {
+    this.name = name;
+    this.speed = speed;
+    this.strength = strength;
+    this.energy = energy;
+  }
+
+
+}
